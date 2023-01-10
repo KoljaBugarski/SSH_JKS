f4e2d5d31b4934f2a3f093d972f001553ca9700f
Kolja Bugarski <bugarskikolja@gmail.com>
2023-01-10 14:21:11 +0100

    Novi kod

--- Kodovi/Moji/sta_je_brze.m
+++ Kodovi/Moji/sta_je_brze.m
@@ -4,11 +4,18 @@ clc
 %%
 v1=1; % koeficijent sprege za 3 talasovoda
 n=11; % broj talasovoda 
-v2=2; % koeficijent sprege za n talasovoda
+v2=1; % koeficijent sprege za n talasovoda
 
-t1=f_transfer12(v1); % vreme/duzina potrebna da se desi 50/50 za 3 talasovoda
-t2=f_transfer12(v2); % vreme/duzina potrebna da se desi 50/50 za 3 talasovoda ali ovde posle nastavljamo propagaciju
-t3=f_transfer11(v2); % vreme/duzina potrebna da se u potpunosti transferuje snaga iz jednog talasovada u drugi
+t_pocetak=0;
+t_kraj=50;
+dt=0.001;
+n_t=uint32((t_kraj-t_pocetak)/dt+1);
+t=linspace(t_pocetak,t_kraj,n_t);   % vremenski/duzinski opseg
+
+t1=f_transfer12(v1,t); % vreme/duzina potrebna da se desi 50/50 za 3 talasovoda
+[t2,vek_1x3]=f_transfer12(v2,t); % vreme/duzina potrebna da se desi 50/50 za 3 talasovoda ali ovde posle nastavljamo propagaciju
+[t3,vek_1x1]=f_transfer11(v2,t); % vreme/duzina potrebna da se u potpunosti transferuje snaga iz jednog talasovada u drugi
+[t4,vek_1x2]=f_transfer1ujenda_polovina(v2,t); % vreme/duzina potrebna da se u pola snage iz jendog transferuje u drugi talasovod
 
 % provera za 3 talasovoda
 psi_inneisp=[0 1 0]';
@@ -55,3 +62,13 @@ T_tri_talasovoda=t1;
 
 % rabi oscilacije =1
 rabi=t3*v2/(pi/2);
+
+x_1x3=t(1:max(size(vek_1x3)));
+y_1x3=vek_1x3(:,2);
+x_1x1=t(1:max(size(vek_1x1)));
+y_1x1=vek_1x1(:,1);
+x_1x2=t(1:max(size(vek_1x2)));
+y_1x2=vek_1x2(:,1);               % iz fitovanja se vidi da se snaga u talasovodima u koje smo ubacili svetlost menja kao cos^2
+                            % stim sto je u slucaju 1x2 omega=v(koeficijent sprege), a 1x3 w=sqrt(2)*v 
+
+
