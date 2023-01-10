586cfcf9b65277fd953a9f507d988a95b344d465
Kolja Bugarski <bugarskikolja@gmail.com>
2023-01-10 14:24:33 +0100

    Novi kod

--- Kodovi/Moji/f_transfer11.m
+++ Kodovi/Moji/f_transfer11.m
@@ -1,23 +1,19 @@
-function [t1]=f_transfer11(v)
+function [t1,vek]=f_transfer11(v,t)
 % vreme/duzina za totalni transfer na osnovu koeficijenta sprege
 gama=0;
 kubna=0;
 saturaciona=0; % moguce nelinearnosti
 
-t_pocetak=0;
-t_kraj=50;
-dt=0.001;
-n_t=uint32((t_kraj-t_pocetak)/dt+1);
-t=linspace(t_pocetak,t_kraj,n_t);   % vremenski/duzinski opseg
 
 H=[0 v;v 0];
 poc_uslov=[1 0];
 maxx=0;
 i_t=0;
+dt=t(3)-t(2);
 b=1;
 while b
     i_t=i_t+1;
-    tt=[t_pocetak+(i_t-1)*dt i_t*dt];
+    tt=[t(1)+(i_t-1)*dt i_t*dt];
     options = odeset('RelTol',1e-12,'AbsTol',1e-12);
     [ttt,vek_t]=ode45(@f_zavisno_z, tt, poc_uslov, options, H, kubna, saturaciona, gama);  
     poc_uslov=vek_t(max(size(ttt)),:);
@@ -33,4 +29,5 @@ while b
     end
 end
 t1=t(i_t);
+vek=abs(vek_pravi.^2);
 end
--- Kodovi/Moji/f_transfer12.m
+++ Kodovi/Moji/f_transfer12.m
@@ -1,23 +1,19 @@
-function t2=f_transfer12(v)
+function [t2,vek]=f_transfer12(v,t)
 % vreme/duzina za 50/50
 gama=0;
 kubna=0;
 saturaciona=0; % moguce nelinearnosti
 
-t_pocetak=0;
-t_kraj=100;
-dt=0.001;
-n_t=uint32((t_kraj-t_pocetak)/dt+1);
-t=linspace(t_pocetak,t_kraj,n_t);   % vremenski/duzinski opseg
 
 H=[0 v 0;v 0 v;0 v 0];
 poc_uslov=[0 1 0];
 i_t=0;
 b=1;
+dt=t(3)-t(2);
 maxx=0;
 while b
     i_t=i_t+1;
-    tt=[t_pocetak+(i_t-1)*dt i_t*dt];
+    tt=[t(1)+(i_t-1)*dt i_t*dt];
     options = odeset('RelTol',1e-12,'AbsTol',1e-12);
     [ttt,vek_t]=ode45(@f_zavisno_z, tt, poc_uslov, options, H, kubna, saturaciona, gama);  
     poc_uslov=vek_t(max(size(ttt)),:);
@@ -33,4 +29,5 @@ while b
     end
 end
 t2=t(i_t-1);
+vek=abs(vek_pravi.^2);
 end
